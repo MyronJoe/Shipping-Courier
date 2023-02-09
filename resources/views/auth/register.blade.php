9b64a3123b9a1ea9178d0d069473fcf0c719db95
Myron <josephonyekachi77@gmail.com>
2023-02-08 17:50:35 -0800

    updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <style>
+        .body{
+            background: red !important;
+            height: 50px;
+        }
+        .min-h-screen{
+            min-height: auto !important;
+            padding: 5em 2em !important;
+        }
+        .logos{
+            font-size: 30px !important;
+            font-weight: 500 !important;
+        }
+        .bg-gray-100{
+            background: none !important;
+        }
+    </style>
+    <!-- All Styles -->
+    @include('frontend.includes.styles')
+</head>
+<body>
+<!-- HEADER -->
+@include('frontend.includes.header')
+<!-- /HEADER -->
+
+<div class="body">
+    <p></p>
+</div>
+
+<div>
+
+
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
@@ -61,3 +100,31 @@
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
+
+
+
+</div>
+
+<!-- Footer Start -->
+@include('frontend.includes.footer')
+<!-- Footer End -->
+
+
+ <!-- Back to Top -->
+ <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-0 back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets/lib/wow/wow.min.js"></script>
+<script src="assets/lib/easing/easing.min.js"></script>
+<script src="assets/lib/waypoints/waypoints.min.js"></script>
+<script src="assets/lib/counterup/counterup.min.js"></script>
+<script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
+
+<!-- Template Javascript -->
+<script src="assets/js/main.js"></script>
+    
+</body>
+</html>
