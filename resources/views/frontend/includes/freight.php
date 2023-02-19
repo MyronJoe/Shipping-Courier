ad0368f84a323c0b980307f4db1987d00d2409ed
Myron <josephonyekachi77@gmail.com>
2023-02-18 22:43:03 -0800

    updated

--- resources/views/frontend/includes/freight.php
+++ resources/views/frontend/includes/freight.php
@@ -1,57 +1,63 @@
 <div class="container-xxl mb-5 pb-4">
-        <div class="container">
-            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h3 class="mb-3">WHY CHOOSE SHIPTOP</h3>
+    <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+            <h3 class="mb-3">WHY CHOOSE SHIPTOP</h3>
 
-                <p class="mb-4 text">
-                    WITH A CUSTOMER FOCUSED BUSINESS MODEL. WE OFFER GREAT SUPPORT AND PRICES FOR ALL TYPES OF ENTERPRENEURS AND BUSINESSES.
-                </p>
+            <p class="mb-4 text">
+                WITH A CUSTOMER FOCUSED BUSINESS MODEL. WE OFFER GREAT SUPPORT AND PRICES FOR ALL TYPES OF ENTERPRENEURS AND BUSINESSES.
+            </p>
 
+        </div>
+        <div class="row g-4">
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="price-item text-center">
+                    <div class="border-bottom p-3 mb-3">
+                        <p><i class="fa fa-plane fa-3x text-primary"></i></p>
+                        <a href="/air">
+                            <h5 class="text-primary">AIR FREIGHT</h5>
+                        </a>
+                    </div>
+                    <div class="p-4 pt-0">
+                        <p>
+                            We provide an integrated network of air and ground transport options that give you the widest range of air cargo choices - all without having to make multiple calls to find the best rates.
+                        </p>
+
+                    </div>
+                </div>
             </div>
-            <div class="row g-4">
-                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="price-item text-center">
-                        <div class="border-bottom p-3 mb-3">
-                            <p><i class="fa fa-plane fa-3x text-primary"></i></p>
-                            <a href="/air"><h5 class="text-primary">AIR FREIGHT</h5></a>
-                        </div>
-                        <div class="p-4 pt-0">
-                            <p>
-                                We provide an integrated network of air and ground transport options that give you the widest range of air cargo choices - all without having to make multiple calls to find the best rates.
-                            </p>
-                            
-                        </div>
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="price-item text-center">
+                    <div class="border-bottom p-3 mb-3">
+                        <i style="transform: scaleX(-1)" class="fa fa-truck fa-3x text-primary mb-3"></i>
+                        <a href="/road">
+                            <h5 class="text-primary">ROAD FREIGHT</h5>
+                        </a>
+                    </div>
+                    <div class="p-4 pt-0">
+                        <p>
+                            Road Freight is freight carried by trucks on the road network. The services are fast, reliable and cost-effective.
+                        </p>
+
                     </div>
                 </div>
-                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="price-item text-center">
-                        <div class="border-bottom p-3 mb-3">
-                            <i style="transform: scaleX(-1)" class="fa fa-truck fa-3x text-primary mb-3"></i>
-                            <a href="/road"><h5 class="text-primary">ROAD FREIGHT</h5></a>
-                        </div>
-                        <div class="p-4 pt-0">
-                            <p>
-                                Road Freight is freight carried by trucks on the road network. The services are fast, reliable and cost-effective.
-                            </p>
-                            
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="price-item text-center">
-                        <div class="border-bottom p-3 mb-3">
-                            <p><i class="fa fa-ship fa-3x text-primary"></i></p>
-                            <a href="/warehouse"><h5 class="text-primary">WAREHOUSE</h5></a>
-                        </div>
-                        <div class="p-4 pt-0">
-                            <p>
-                                We offer warehouses at different locations in and outside the United States for storing of goods and package. Contact support to inquire and rent any of our warehouses.
-                            </p>
-                            
-                        </div>
-                    </div>
-                </div>
-        
             </div>
+            <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="price-item text-center">
+                    <div class="border-bottom p-3 mb-3">
+                        <p><i class="fa fa-ship fa-3x text-primary"></i></p>
+                        <a href="/warehouse">
+                            <h5 class="text-primary">WAREHOUSE</h5>
+                        </a>
+                    </div>
+                    <div class="p-4 pt-0">
+                        <p>
+                            We offer warehouses at different locations in and outside the United States for storing of goods and package. Contact support to inquire and rent any of our warehouses.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
+</div>
