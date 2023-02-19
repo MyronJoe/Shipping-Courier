271e8c42f9507b34a47df7965b5dc65998f0d2ab
Myron <josephonyekachi77@gmail.com>
2023-02-18 22:41:16 -0800

    updated

--- resources/views/frontend/includes/carousel.php
+++ resources/views/frontend/includes/carousel.php
@@ -1,34 +1,34 @@
 <div class="container-fluid p-0 ">
-        <div class="owl-carousel header-carousel position-relative mb-5">
-            <div class="owl-carousel-item position-relative">
-                <img class="img-fluid" src="assets/img/carousel-1.jpg" alt="">
-                <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(6, 3, 21, .5);">
-                    <div class="container">
-                        <div class="row justify-content-start">
-                            <div class="col-10 col-lg-8">
-                                <h5 class="text-warning text-uppercase mb-3 animated slideInDown">Transport & Logistics Solution</h5>
-                                <h1 style="text-transform: uppercase;" class="display-3 text-white animated slideInDown mb-4 bold">world most Fastest secure delivery service</h1>
-                                <a href="/tracking" class="noRadius btn btn-warning text-light py-md-3 px-md-5 me-3 animated slideInLeft mt-2">TRACK SHIPMENT</a>
-                                <a href="/contact" class="noRadius btn btn-outline-warning text-light py-md-3 px-md-5 animated slideInRight mt-2">CONTACT US</a>
-                            </div>
+    <div class="owl-carousel header-carousel position-relative mb-5">
+        <div class="owl-carousel-item position-relative">
+            <img class="img-fluid" src="assets/img/carousel-1.jpg" alt="">
+            <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(6, 3, 21, .5);">
+                <div class="container">
+                    <div class="row justify-content-start">
+                        <div class="col-10 col-lg-8">
+                            <h5 class="text-warning text-uppercase mb-3 animated slideInDown">Transport & Logistics Solution</h5>
+                            <h1 style="text-transform: uppercase;" class="display-3 text-white animated slideInDown mb-4 bold">world most Fastest secure delivery service</h1>
+                            <a href="/tracking" class="noRadius btn btn-warning text-light py-md-3 px-md-5 me-3 animated slideInLeft mt-2">TRACK SHIPMENT</a>
+                            <a href="/contact" class="noRadius btn btn-outline-warning text-light py-md-3 px-md-5 animated slideInRight mt-2">CONTACT US</a>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="owl-carousel-item position-relative">
-                <img class="img-fluid" src="assets/img/carousel-2.jpg" alt="">
-                <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(6, 3, 21, .5);">
-                    <div class="container">
-                        <div class="row justify-content-start">
-                            <div class="col-10 col-lg-8">
-                                <h5 class="text-warning text-uppercase mb-3 animated slideInDown ">Transport & Logistics Solution</h5>
-                                <h1 style="text-transform: uppercase;" class="display-3 text-white animated slideInDown mb-4 bold">#1 Place For Your Transport Solution</h1>
-                                <a href="/tracking" class="noRadius btn btn-warning text-light py-md-3 px-md-5 me-3 animated slideInLeft mt-2">TRACK SHIPMENT</a>
-                                <a href="/contact" class="noRadius btn btn-outline-warning text-light py-md-3 px-md-5 animated slideInRight mt-2">CONTACT US</a>
-                            </div>
+        </div>
+        <div class="owl-carousel-item position-relative">
+            <img class="img-fluid" src="assets/img/carousel-2.jpg" alt="">
+            <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(6, 3, 21, .5);">
+                <div class="container">
+                    <div class="row justify-content-start">
+                        <div class="col-10 col-lg-8">
+                            <h5 class="text-warning text-uppercase mb-3 animated slideInDown ">Transport & Logistics Solution</h5>
+                            <h1 style="text-transform: uppercase;" class="display-3 text-white animated slideInDown mb-4 bold">#1 Place For Your Transport Solution</h1>
+                            <a href="/tracking" class="noRadius btn btn-warning text-light py-md-3 px-md-5 me-3 animated slideInLeft mt-2">TRACK SHIPMENT</a>
+                            <a href="/contact" class="noRadius btn btn-outline-warning text-light py-md-3 px-md-5 animated slideInRight mt-2">CONTACT US</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
