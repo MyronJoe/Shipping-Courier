3d34b33010d6d5b876f103bd87fbbabba98f13bb
Myron <josephonyekachi77@gmail.com>
2023-02-18 22:42:02 -0800

    updated

--- resources/views/frontend/includes/feature.php
+++ resources/views/frontend/includes/feature.php
@@ -1,37 +1,37 @@
 <div class="container-fluid overflow-hidden py-4 px-lg-0">
-        <div class="container feature py-5 px-lg-0">
-            <div class="row g-5 mx-lg-0">
-                <div class="col-lg-6 feature-text wow fadeInUp" data-wow-delay="0.1s">
-                    <h6 class="text-secondary text-uppercase mb-3">Our Features</h6>
-                    <h1 class="mb-5">WE OFFER BEST LOGISTIC PROVIDER FOR OUR CLIENTS</h1>
-                    <p>WE PROVIDE THE BEST LOGISTIC SERVICE ON THE MARKET, ALL OVER THE GLOBE IN CASE YOU HAVE AN ENQUIRY, OR AN URGENT QUESTION, OUR SUPPORT IS THERE.</p>
-                    <div class="d-flex mb-5 wow fadeInUp" data-wow-delay="0.3s">
-                        <i class="fa fa-globe text-primary fa-3x flex-shrink-0"></i>
-                        <div class="ms-4">
-                            <h5>Worldwide Service</h5>
-                            <p class="mb-0">We provide the best logistic service for globally</p>
-                        </div>
-                    </div>
-                    <div class="d-flex mb-5 wow fadeIn" data-wow-delay="0.5s">
-                        <i class="fa fa-clock-o text-primary fa-3x flex-shrink-0"></i>
-                        <div class="ms-4">
-                            <h5>On Time Delivery</h5>
-                            <p class="mb-0">We know how to make it in time and set the right terms for deliveries.</p>
-                        </div>
-                    </div>
-                    <div class="d-flex mb-0 wow fadeInUp" data-wow-delay="0.7s">
-                        <i class="fa fa-credit-card-alt text-primary fa-3x flex-shrink-0"></i>
-                        <div class="ms-4">
-                            <h5>Payment Method</h5>
-                            <p class="mb-0">All payment methods are acceptable for ordering our services.</p>
-                        </div>
+    <div class="container feature py-5 px-lg-0">
+        <div class="row g-5 mx-lg-0">
+            <div class="col-lg-6 feature-text wow fadeInUp" data-wow-delay="0.1s">
+                <h6 class="text-secondary text-uppercase mb-3">Our Features</h6>
+                <h1 class="mb-5">WE OFFER BEST LOGISTIC PROVIDER FOR OUR CLIENTS</h1>
+                <p>WE PROVIDE THE BEST LOGISTIC SERVICE ON THE MARKET, ALL OVER THE GLOBE IN CASE YOU HAVE AN ENQUIRY, OR AN URGENT QUESTION, OUR SUPPORT IS THERE.</p>
+                <div class="d-flex mb-5 wow fadeInUp" data-wow-delay="0.3s">
+                    <i class="fa fa-globe text-primary fa-3x flex-shrink-0"></i>
+                    <div class="ms-4">
+                        <h5>Worldwide Service</h5>
+                        <p class="mb-0">We provide the best logistic service for globally</p>
                     </div>
                 </div>
-                <div class="col-lg-6 pe-lg-0 wow fadeInRight" data-wow-delay="0.1s" style="min-height: 400px;">
-                    <div class="position-relative h-100">
-                        <img class="position-absolute img-fluid w-100 h-100" src="assets/img/feature.jpg" style="object-fit: cover;" alt="">
+                <div class="d-flex mb-5 wow fadeIn" data-wow-delay="0.5s">
+                    <i class="fa fa-clock-o text-primary fa-3x flex-shrink-0"></i>
+                    <div class="ms-4">
+                        <h5>On Time Delivery</h5>
+                        <p class="mb-0">We know how to make it in time and set the right terms for deliveries.</p>
                     </div>
+                </div>
+                <div class="d-flex mb-0 wow fadeInUp" data-wow-delay="0.7s">
+                    <i class="fa fa-credit-card-alt text-primary fa-3x flex-shrink-0"></i>
+                    <div class="ms-4">
+                        <h5>Payment Method</h5>
+                        <p class="mb-0">All payment methods are acceptable for ordering our services.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 pe-lg-0 wow fadeInRight" data-wow-delay="0.1s" style="min-height: 400px;">
+                <div class="position-relative h-100">
+                    <img class="position-absolute img-fluid w-100 h-100" src="assets/img/feature.jpg" style="object-fit: cover;" alt="">
                 </div>
             </div>
         </div>
     </div>
+</div>
