6b8ddefa191c073159c545d631e973e5ce473e89
Myron <josephonyekachi77@gmail.com>
2023-02-20 22:53:46 -0800

    updated

--- resources/views/frontend/trackdetails.blade.php
+++ resources/views/frontend/trackdetails.blade.php
@@ -7,35 +7,35 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
-    
+
     <!-- Favicon -->
     <link href="assets/img/favicon.ico" rel="icon">
 
     <!-- All Styles -->
     @include('frontend.includes.styles')
-    
+
 </head>
 
 <body>
-    
+
     <!-- HEADER -->
     @include('frontend.includes.header')
-	<!-- /HEADER -->
+    <!-- /HEADER -->
 
 
     <div class="content">
 
-    @foreach($track as $data)
+        @foreach($track as $data)
 
         <h1>{{$data->status}}</h1>
 
-    @endforeach
+        @endforeach
 
 
 
-    <!-- Footer Start -->
-    @include('frontend.includes.footer')
-    <!-- Footer End -->
+        <!-- Footer Start -->
+        @include('frontend.includes.footer')
+        <!-- Footer End -->
 
 
 
