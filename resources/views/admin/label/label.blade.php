e1920222d96e3cacd5de93239622260334bf7597
Myron <josephonyekachi77@gmail.com>
2023-03-07 17:03:51 -0800

    updated

--- resources/views/admin/label/label.blade.php
+++ resources/views/admin/label/label.blade.php
@@ -1,12 +1,71 @@
 <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
     <title>{{$data->product}}</title>
+
+    <!-- STYLES -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="shortcut icon" href="../backend/img/icons/icon-48x48.png" />
+
+    <link rel="canonical" href="https://demo-basic.adminkit.io/" />
+
+    <link href="../backend/css/app.css" rel="stylesheet">
+    <link href="../backend/css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+
 </head>
+
 <body>
-    <h1>{{$data->product}}</h1>
+    <div class="wrapper">
+        <!-- NAV-SIDE -->
+        @include('admin.includes.sidebar')
+
+        <div class="main">
+            <!-- NAV-TOP -->
+            @include('admin.includes.nav')
+
+            <main class="content">
+
+                
+
+                <div class="row">
+
+                    <div class="col-sm-12 col-md-6">
+                        <h1>Logistica</h1>
+                    </div>
+
+                    <div class="col-sm-12 col-md-6">
+
+                        <h1>{{$data->product}}</h1>
+
+                    </div>
+
+                </div>
+
+    
+
+            </main>
+            <!-- FOOTER -->
+            @include('admin.includes.footer')
+        </div>
+    </div>
+
+    <script src="../backend/js/app.js"></script>
+
+
 </body>
+
 </html>
