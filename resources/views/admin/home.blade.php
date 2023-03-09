4473898d927bbaf522acc6e88d2c663aaf2f082e
Myron <josephonyekachi77@gmail.com>
2023-03-09 12:20:58 -0800

    updated

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -48,8 +48,9 @@
 						<div class="col-12 d-flex">
 							<div class="w-100">
 								<div class="row">
-									<div class="col-sm-6">
-										<div class="card">
+									<div class="row">
+										<div class="p-2 col-sm-12 col-md-6 col-xl-3">
+											<div class="card">
 											<div class="card-body">
 												<div class="row">
 													<div class="col mt-0">
@@ -65,8 +66,10 @@
 												<h1 class="mt-1 mb-3">{{$shipments}}</h1>
 	
 											</div>
+											</div>
 										</div>
-										<div class="card">
+										<div class="p-2 col-sm-12 col-md-6 col-xl-3">
+											<div class="card">
 											<div class="card-body">
 												<div class="row">
 													<div class="col mt-0">
@@ -82,10 +85,29 @@
 												<h1 class="mt-1 mb-3">{{$users}}</h1>
 												
 											</div>
+											</div>
 										</div>
-									</div>
-									<div class="col-sm-6">
-										<div class="card">
+										<div class="p-2 col-sm-12 col-md-6 col-xl-3">
+											<div class="card">
+											<div class="card-body">
+												<div class="row">
+													<div class="col mt-0">
+														<h5 class="card-title">Deliverd Shipments</h5>
+													</div>
+
+													<div class="col-auto">
+														<div class="stat text-primary">
+															<i class="align-middle" data-feather="shopping-cart"></i>
+														</div>
+													</div>
+												</div>
+												<h1 class="mt-1 mb-3">BB</h1>
+
+											</div>
+											</div>
+										</div>
+										<div class="p-2 col-sm-12 col-md-6 col-xl-3">
+											<div class="card">
 											<div class="card-body">
 												<div class="row">
 													<div class="col mt-0">
@@ -100,29 +122,10 @@
 												</div>
 												<h1 class="mt-1 mb-3">{{$paid}}</h1>
 											</div>
-										</div>
-										<div class="card">
-											<div class="card-body">
-												<div class="row">
-													<div class="col mt-0">
-														<h5 class="card-title">Orders</h5>
-													</div>
-
-													<div class="col-auto">
-														<div class="stat text-primary">
-															<i class="align-middle" data-feather="shopping-cart"></i>
-														</div>
-													</div>
-												</div>
-												<h1 class="mt-1 mb-3">64</h1>
-												<div class="mb-0">
-													<span class="text-danger"> <i
-															class="mdi mdi-arrow-bottom-right"></i> -2.25% </span>
-													<span class="text-muted">Since last week</span>
-												</div>
 											</div>
 										</div>
 									</div>
+
 								</div>
 							</div>
 						</div>
