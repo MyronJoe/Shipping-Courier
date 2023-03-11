0f3e5cc06cccca25268014b22e238c5d6297680c
Myron <josephonyekachi77@gmail.com>
2023-03-10 22:24:26 -0800

    updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -108,7 +108,10 @@ class AdminController extends Controller
 
         $data->save();
 
-        return redirect('shipments')->with('message', 'Shipment Added Successfully');
+        // return redirect('shipments')->with('message', 'Shipment Added Successfully');
+
+        Alert::success('Shipment Added Successfully');
+        return redirect('shipments');
     }
 
     //Delete shipment function
