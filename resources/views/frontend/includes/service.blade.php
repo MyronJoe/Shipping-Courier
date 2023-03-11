32febd510fc6e10bb50dfd0c86e2942f116e030f
Myron <josephonyekachi77@gmail.com>
2023-03-10 21:49:54 -0800

    updated

--- resources/views/frontend/includes/service.blade.php
+++ resources/views/frontend/includes/service.blade.php
@@ -13,7 +13,7 @@
                         </div>
                         <h4 class="mb-3">Air Freight</h4>
                         <p>Fastest method of delivery for your time sensitive packages.</p>
-                        <a class="btn-slide mt-2" href="/air"><i class="fa fa-arrow-right"></i><span>Read More</span></a>
+                        <a class="btn-slide mt-2" href="/services/air"><i class="fa fa-arrow-right"></i><span>Read More</span></a>
                     </div>
                 </div>
                 <div class="col-md-6 col-lg-4 wow fadeInUp" data-wow-delay="0.5s">
