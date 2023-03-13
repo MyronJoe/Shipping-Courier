efe2b5251bc12b59cd861a84f1ae9944fb63dcf0
Myron <josephonyekachi77@gmail.com>
2023-03-12 21:18:17 -0800

    updated

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,69 +1,65 @@
 <nav id="sidebar" class="sidebar js-sidebar">
-			<div class="sidebar-content js-simplebar">
-				<a class="sidebar-brand" href="{{url('/')}}">
-					<span class="align-middle">Logistica</span>
+	<div class="sidebar-content js-simplebar">
+		<a class="sidebar-brand" href="{{url('/')}}">
+			<span class="align-middle">Logistica</span>
+		</a>
+
+		<ul class="sidebar-nav">
+			<li class="sidebar-header">
+				Content Management
+			</li>
+
+			<li class="sidebar-item active">
+				<a class="sidebar-link" href="{{ url('admin')}}">
+					<i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
 				</a>
+			</li>
 
-				<ul class="sidebar-nav">
-					<li class="sidebar-header">
-						Content Management
-					</li>
-
-					<li class="sidebar-item active">
-						<a class="sidebar-link" href="{{ url('admin')}}">
-							<i class="align-middle" data-feather="sliders"></i> <span
-								class="align-middle">Dashboard</span>
-						</a>
-					</li>
-
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="{{ url('shipments')}}">
-							<i class="align-middle" data-feather="check-square"></i> <span
-								class="align-middle">Shipments</span>
-						</a>
-					</li>
-					
-
-					<li class="sidebar-header">
-						Page Components
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="{{ url('shipments')}}">
+					<i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Shipments</span>
+				</a>
+			</li>
 
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-cards.html">
-							<i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
-						</a>
-					</li>
+			<li class="sidebar-header">
+				Page Components
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="ui-typography.html">
-							<i class="align-middle" data-feather="align-left"></i> <span
-								class="align-middle">Typography</span>
-						</a>
-					</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="icons-feather.html">
-							<i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
-						</a>
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="ui-cards.html">
+					<i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
+				</a>
+			</li>
 
-					<li class="sidebar-header">
-						Plugins & Addons
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="ui-typography.html">
+					<i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
+				</a>
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="charts-chartjs.html">
-							<i class="align-middle" data-feather="bar-chart-2"></i> <span
-								class="align-middle">Charts</span>
-						</a>
-					</li>
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="icons-feather.html">
+					<i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
+				</a>
+			</li>
 
-					<li class="sidebar-item">
-						<a class="sidebar-link" href="maps-google.html">
-							<i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
-						</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
+			<li class="sidebar-header">
+				Plugins & Addons
+			</li>
+
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="charts-chartjs.html">
+					<i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
+				</a>
+			</li>
+
+			<li class="sidebar-item">
+				<a class="sidebar-link" href="maps-google.html">
+					<i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+</nav>
