c24be713ba56b8e19ad9b951904dca95b27f0c6c
Myron <josephonyekachi77@gmail.com>
2023-03-25 22:17:36 -0800

    updated

--- database/migrations/2023_03_26_061403_create_messages_table.php
+++ database/migrations/2023_03_26_061403_create_messages_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->string('subject')->nullable();
+            $table->string('details')->nullable();
             $table->timestamps();
         });
     }
