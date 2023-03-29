744e3994844eed725f208ff565bc55d4e387247f
Myron <josephonyekachi77@gmail.com>
2023-03-29 04:00:46 -0800

    updated

--- resources/views/admin/site/features/add_features.blade.php
+++ resources/views/admin/site/features/add_features.blade.php
@@ -76,7 +76,7 @@
                                 @enderror
                             </div>
 
-                            <input type="submit" name="add_carousel" value="Add Features" class="btn btn-primary my-3 " >
+                            <input type="submit" name="add_carousel" value="Add Features" class="btn btn-primary my-3 ">
                         </div>
                     </div>
                 </main>
