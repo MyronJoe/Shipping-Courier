a167430278a5d085cbab48ddbfe25a1686f5aa2a
Myron <josephonyekachi77@gmail.com>
2023-04-03 12:57:24 -0800

    updated

--- resources/views/frontend/includes/feature.blade.php
+++ resources/views/frontend/includes/feature.blade.php
@@ -7,24 +7,25 @@
 
                 <h6 class="text-secondary text-uppercase mb-3">Our Features</h6>
                 <h1 class="mb-5">{{$header->feature_title}}</h1>
-                <p>{{$header->feature_sub}}</p>
+                <p class="mb-5">{{$header->feature_sub}}</p>
 
                 @endif
             @endforeach
 
-            @foreach($header as $key => $header)
+            @foreach($features as $key => $features)
             @if($key < 3) 
-                <div class="d-flex mb-5 wow fadeInUp" data-wow-delay="0.3s">
-                    <i class="fa fa-globe text-primary fa-3x flex-shrink-0"></i>
+                <div class="d-flex mb-5 wow fadeInUp flx" data-wow-delay="0.3s">
+                    <!-- <i class="fa fa-globe text-primary fa-3x flex-shrink-0"></i> -->
+                    <img class="features-icon flex-shrink-0" src="assets/img/{{$features->icon}}" alt="">
                     <div class="ms-4">
-                        <h5>Worldwide Service</h5>
-                        <p class="mb-0">We provide the best logistic service for globally</p>
+                        <h5>{{$features->title}}</h5>
+                        <p class="mb-0">{{$features->sub_title}}</p>
                     </div>
                 </div>
                 @endif
             @endforeach
 
-                <div class="d-flex mb-5 wow fadeIn" data-wow-delay="0.5s">
+                <!-- <div class="d-flex mb-5 wow fadeIn" data-wow-delay="0.5s">
                     <i class="fa fa-clock-o text-primary fa-3x flex-shrink-0"></i>
                     <div class="ms-4">
                         <h5>On Time Delivery</h5>
@@ -37,7 +38,7 @@
                         <h5>Payment Method</h5>
                         <p class="mb-0">All payment methods are acceptable for ordering our services.</p>
                     </div>
-                </div>
+                </div> -->
             </div>
             @foreach($headers as $key => $headers)
             @if($key < 1) 
