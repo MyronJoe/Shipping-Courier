cfd52e82b08dcb8e2b2ca0ad10f6b59bf69d36b7
Myron <josephonyekachi77@gmail.com>
2023-04-05 21:29:54 -0800

    updated

--- resources/views/frontend/includes/carousel.blade.php
+++ resources/views/frontend/includes/carousel.blade.php
@@ -19,20 +19,5 @@
         </div>
         @endif
     @endforeach
-        <!-- <div class="owl-carousel-item position-relative">
-            <img class="img-fluid" src="assets/img/carousel-2.jpg" alt="">
-            <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(6, 3, 21, .5);">
-                <div class="container">
-                    <div class="row justify-content-start">
-                        <div class="col-10 col-lg-8">
-                            <h5 class="text-warning text-uppercase mb-3 animated slideInDown ">Transport & Logistics Solution</h5>
-                            <h1 style="text-transform: uppercase;" class="display-3 text-white animated slideInDown mb-4 bold">#1 Place For Your Transport Solution</h1>
-                            <a href="/tracking" class="noRadius btn btn-warning text-light py-md-3 px-md-5 me-3 animated slideInLeft mt-2">TRACK SHIPMENT</a>
-                            <a href="/contact" class="noRadius btn btn-outline-warning text-light py-md-3 px-md-5 animated slideInRight mt-2">CONTACT US</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> -->
     </div>
 </div>
